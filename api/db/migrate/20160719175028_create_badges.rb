db60aa2d3ad72c603334b5beb7719cc19f8d8d30
Ben & Jonathan Hall <pair+benjaminhyw.jlhall@devbootcamp.com>
2016-07-20 10:00:36 -0700

    Add null: false to description, add default: 0 to votes

--- api/db/migrate/20160719175028_create_badges.rb
+++ api/db/migrate/20160719175028_create_badges.rb
@@ -2,8 +2,8 @@ class CreateBadges < ActiveRecord::Migration[5.0]
   def change
     create_table :badges do |t|
       t.integer :student_id
-      t.string :description
-      t.integer :votes
+      t.string :description, null: false
+      t.integer :votes, default: 0
 
       t.timestamps
     end
