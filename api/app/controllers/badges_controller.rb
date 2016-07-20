32814dfb0d0c4b97d79e4c407106f34c3fc88d2e
Ben & Jonathan Hall <pair+benjaminhyw.jlhall@devbootcamp.com>
2016-07-20 10:01:24 -0700

    Add new method, updated method, and private badge_params method

--- api/app/controllers/badges_controller.rb
+++ api/app/controllers/badges_controller.rb
@@ -5,9 +5,33 @@ class BadgesController < ApplicationController
     render json: @badges
   end
 
+  def new
+    @badge = Badge.new(badge_params)
+    if @badge.save
+      render json: @badge
+    else
+      render json: @badge.errors.full_messages
+    end
+  end
+
+  def update
+    @badge = Badge.find(params[:badge_id])
+    if params[:vote_type] == "up"
+      @badge.votes += 1
+    elsif params[:vote_type] == "down"
+      @badge.votes -=1
+    end
+    @badge.save
+    render json: @badge
+  end
+
   private
     def find_student
       @student = Student.find_by(name: params[:name])
+    end
+
+    def badge_params
+      params.require(:description, :student_id).permit(:description, :vote_type, :student_id)
     end
 
 end
