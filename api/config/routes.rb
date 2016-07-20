3952af3dbe03fc473ce8c0e6f6aeed5b16d2e68e
Ben & Jonathan Hall <pair+benjaminhyw.jlhall@devbootcamp.com>
2016-07-20 09:59:47 -0700

    Add badge routes

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "students#index"
   get '/:name/badges', to: 'badges#index'
   get '/:name', to: 'students#show'
-
+  post '/badges', to: 'badges#new'
+  patch '/badges', to: 'badges#update'
 end
 
