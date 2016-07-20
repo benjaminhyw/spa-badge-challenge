6edd82d034bdde2aaa5d8c8323d767ed18c8d251
Ben & Jonathan Hall <pair+benjaminhyw.jlhall@devbootcamp.com>
2016-07-20 11:48:33 -0700

    Add teacher and badge display

--- app/js/app.js
+++ app/js/app.js
@@ -1,44 +1,65 @@
 $(document).ready( function() {
 // INDEX
+getTeachers();
+getTeacher();
+vote();
+addBadge();
+
+function teachersTemplate(response){
+  var teachersTemplateScript = $("#teachers_template").html();
+  var teachersTemplate = Handlebars.compile(teachersTemplateScript);
+  var context = {
+    data: response
+  }
+  var render = teachersTemplate(context);
+    $('#main').html(render);
+}
 
 function getTeachers() {
-$('body').on('click', '#teacher-list', function(event) {
-  event.preventDefault();
-  $.ajax({
-    method: "GET",
-    url: "http://sample-badges-api.herokuapp.com/teachers",
-    dataType: "json"
-  }).done(function(response){
-    console.log(response);
-    // use template to format and display or something?
+  $('body').on('click', '#teacher-list', function(event) {
+    event.preventDefault();
+    $.ajax({
+      method: "GET",
+      url: "http://sample-badges-api.herokuapp.com/teachers",
+      dataType: "json"
+    }).done(function(response){
+      console.log(response);
+      teachersTemplate(response);
+    });
   });
-});
 };
 
+function teachersBadgesTemplate(response){
+  var teachersBadgesTemplateScript = $("#badges_template").html();
+  var teachersBadgesTemplate = Handlebars.compile(teachersBadgesTemplateScript);
+  var context = {
+    data: response
+  }
+  var render = teachersBadgesTemplate(context);
+    $('#main').html(render);
+}
+
 function getTeacher() {
-$('body').on('click', '#teacher', function(event) {
+$('body').on('click', '#badges', function(event) {
   event.preventDefault();
-  var teacherID = $(this).attr('id');
+  var teacherID = $(this).closest("li").attr('id');
   $.ajax({
     method: "GET",
     url: "http://sample-badges-api.herokuapp.com/teachers/" + teacherID,
     dataType: "json"
   }).done(function(response){
     console.log(response);
-    // use template to format and display or something?
+    teachersBadgesTemplate(response)
   });
 });
 };
-
-// SAMPLE PAGE
-
 
 function vote() {
 $('.slogan').on('click', 'button', function(event) {
   event.preventDefault();
   var currentBadgeID = $(this).closest('div').parent().attr('id');
   var voteType = $(this).attr('class');
-  var voteData = '{"vote": {"id": "' + badgeID + '", "vote_type": "' + voteType + '"}}'
+  var voteData = '{"vote": {"id": "' + currentBadgeID + '", "vote_type": "' + voteType + '"}}'
   console.log(voteData);
   $.ajax({
     method: "PUT",
@@ -55,13 +76,16 @@ $('.slogan').on('click', 'button', function(event) {
 function addBadge() {
 $('.add-badge').on('click', 'input[type="image"]', function(event) {
   event.preventDefault();
-  var badgeData = $(this).closest('form').serialize();
-  // var badgeData = '{"badge": {"phrase": "' + currentBadgeID + '", "vote_type": "' + voteType + '"}}'
+  var currentBadgeID = $(this).closest('div').parent().attr('id');
+  var voteType = $(this).attr('class');
+
+  // var badgeData = $(this).closest('form').serialize();
+  var badgeData = '{"badge": {"phrase": "' + currentBadgeID + '", "vote_type": "' + voteType + '"}}'
   // we'll iterate on this tomorrow
-  var badgeData = JSON.stringify(badgeData);
+  // var badgeData = JSON.stringify(badgeData);
   $.ajax({
     method: "POST",
-    url: "http://sample-badges-api.herokuapp.com/badges"
+    url: "http://sample-badges-api.herokuapp.com/badges",
     data: badgeData,
     dataType: "json"
   }).done(function(response){
