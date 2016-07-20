8a9031ffddb39fa66c448756f83306ed22470d92
Ben & Jonathan Hall <pair+benjaminhyw.jlhall@devbootcamp.com>
2016-07-20 09:36:34 -0700

    Add an index method, add a private method to fund_student, call a before_action :find_student

--- api/app/controllers/badges_controller.rb
+++ api/app/controllers/badges_controller.rb
@@ -1,2 +1,13 @@
 class BadgesController < ApplicationController
+  before_action :find_student
+  def index
+    @badges = @student.badges
+    render json: @badges
+  end
+
+  private
+    def find_student
+      @student = Student.find_by(name: params[:name])
+    end
+
 end
