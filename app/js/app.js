a8f2b3392c5e29b3a60ebe46ff93974de628ec6d
Ben & Jonathan Hall <pair+benjaminhyw.jlhall@devbootcamp.com>
2016-07-20 12:30:41 -0700

    stopping point, cross origin error

--- app/js/app.js
+++ app/js/app.js
@@ -1,100 +1,100 @@
 $(document).ready( function() {
 // INDEX
-getTeachers();
-getTeacher();
-vote();
-addBadge();
+getStudents();
+getStudent();
+// vote();
+// addBadge();
 
-function teachersTemplate(response){
-  var teachersTemplateScript = $("#teachers_template").html();
-  var teachersTemplate = Handlebars.compile(teachersTemplateScript);
+function studentsTemplate(response){
+  var studentsTemplateScript = $("#students_template").html();
+  var studentsTemplate = Handlebars.compile(studentsTemplateScript);
   var context = {
     data: response
   }
-  var render = teachersTemplate(context);
+  var render = studentsTemplate(context);
     $('#main').html(render);
 }
 
-function getTeachers() {
-  $('body').on('click', '#teacher-list', function(event) {
+function getStudents() {
+  $('body').on('click', '#student-list', function(event) {
     event.preventDefault();
     $.ajax({
       method: "GET",
-      url: "http://sample-badges-api.herokuapp.com/teachers",
+      url: "http://www.localhost:3000/students",
       dataType: "json"
     }).done(function(response){
       console.log(response);
-      teachersTemplate(response);
+      studentsTemplate(response);
     });
   });
 };
 
-function teachersBadgesTemplate(response){
-  var teachersBadgesTemplateScript = $("#badges_template").html();
-  var teachersBadgesTemplate = Handlebars.compile(teachersBadgesTemplateScript);
+function studentsBadgesTemplate(response){
+  var studentsBadgesTemplateScript = $("#badges_template").html();
+  var studentsBadgesTemplate = Handlebars.compile(studentsBadgesTemplateScript);
   var context = {
     data: response
   }
-  var render = teachersBadgesTemplate(context);
+  var render = studentsBadgesTemplate(context);
     $('#main').html(render);
 }
 
-function getTeacher() {
+function getStudent() {
 $('body').on('click', '#badges', function(event) {
   event.preventDefault();
-  var teacherID = $(this).closest("li").attr('id');
+  var studentID = $(this).closest("li").attr('id');
   $.ajax({
     method: "GET",
-    url: "http://sample-badges-api.herokuapp.com/teachers/" + teacherID,
+    url: "http://www.localhost:3000/students/" + studentID,
     dataType: "json"
   }).done(function(response){
     console.log(response);
-    teachersBadgesTemplate(response)
+    studentsBadgesTemplate(response)
   });
 });
 };
 
-function vote() {
-$('.test').on('click', 'button', function(event) {
-  event.preventDefault();
-  var currentBadgeID = $(this).attr('class').value()
-  console.log(currentBadgeID)
-  var voteType = $(this).attr('class');
-  var voteData = '{"vote": {"id": "' + currentBadgeID + '", "vote_type": "' + voteType + '"}}'
-  console.log(voteData);
-  $.ajax({
-    method: "PUT",
-    url: "http://sample-badges-api.herokuapp.com/badges/" + currentBadgeID,
-    data: voteData,
-    dataType: "json"
-  }).done(function(response){
-    console.log(response);
-    // should get success back here
-  });
-});
-};
+// function vote() {
+// $('.test').on('click', 'button', function(event) {
+//   event.preventDefault();
+//   var currentBadgeID = $(this).attr('class').value()
+//   console.log(currentBadgeID)
+//   var voteType = $(this).attr('class');
+//   var voteData = '{"vote": {"id": "' + currentBadgeID + '", "vote_type": "' + voteType + '"}}'
+//   console.log(voteData);
+//   $.ajax({
+//     method: "PUT",
+//     url: "http://www.localhost:3000/badges/" + currentBadgeID,
+//     data: voteData,
+//     dataType: "json"
+//   }).done(function(response){
+//     console.log(response);
+//     // should get success back here
+//   });
+// });
+// };
 
-function addBadge() {
-$('.add-badge').on('click', 'input[type="image"]', function(event) {
-  event.preventDefault();
-  var currentBadgeID = $(this).closest('div').parent().attr('id');
-  var voteType = $(this).attr('class');
+// function addBadge() {
+// $('.add-badge').on('click', 'input[type="image"]', function(event) {
+//   event.preventDefault();
+//   var currentBadgeID = $(this).closest('div').parent().attr('id');
+//   var voteType = $(this).attr('class');
 
-  // var badgeData = $(this).closest('form').serialize();
-  var badgeData = '{"badge": {"phrase": "' + currentBadgeID + '", "vote_type": "' + voteType + '"}}'
-  // we'll iterate on this tomorrow
-  // var badgeData = JSON.stringify(badgeData);
-  $.ajax({
-    method: "POST",
-    url: "http://sample-badges-api.herokuapp.com/badges",
-    data: badgeData,
-    dataType: "json"
-  }).done(function(response){
-    console.log(response);
-    // this needs to add the badge to the "owner's page"
-  });
-});
-};
+//   // var badgeData = $(this).closest('form').serialize();
+//   var badgeData = '{"badge": {"phrase": "' + currentBadgeID + '", "vote_type": "' + voteType + '"}}'
+//   // we'll iterate on this tomorrow
+//   // var badgeData = JSON.stringify(badgeData);
+//   $.ajax({
+//     method: "POST",
+//     url: "http://www.localhost:3000/badges",
+//     data: badgeData,
+//     dataType: "json"
+//   }).done(function(response){
+//     console.log(response);
+//     // this needs to add the badge to the "owner's page"
+//   });
+// });
+// };
 
 });
 
